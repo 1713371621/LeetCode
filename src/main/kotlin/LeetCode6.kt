c51d08e113cb53f53d942ec55b29a9a8a1a98436
shijiayao <1713371621@qq.com>
2020-09-16 02:24:47 +0800

    feat: LeetCode6.kt

--- src/main/kotlin/LeetCode6.kt
+++ src/main/kotlin/LeetCode6.kt
@@ -1,5 +1,3 @@
-import com.sun.xml.internal.fastinfoset.util.StringArray
-
 /**
  *
  * @author shijiayao
@@ -16,31 +14,28 @@ class LeetCode6 {
     }
 
     fun convert(s: String, numRows: Int): String {
-        val z: Array<CharArray> = Array(s.length) { CharArray(numRows) }
-        for (i: Int in s.indices) {
-            for (j: Int in 0 until numRows) {
-                z[i][j] = ' '
-            }
+        if (numRows < 2) {
+            return s
         }
-        val charArray: CharArray = s.toCharArray()
-        var j = 0
-        for (i: Int in s.indices) {
-            if (j >= numRows) {
-                j = 0
-            }
-            z[(i - j) / 2][j] = charArray[i]
-            j++
+        val z: Array<StringBuffer> = Array(numRows) { StringBuffer() }
+        for (i in 0 until numRows) {
+            z[i] = StringBuffer()
         }
-        val stringBuffer = StringBuffer()
-            for (i: Int in s.indices) {
-                for (k: Int in 0 until numRows) {
 
-                    print(z[i][k])
-                if (z[i][k] != ' ') {
-                    stringBuffer.append(z[i][k])
-                }
+        val charArray: CharArray = s.toCharArray()
+        var index = 0
+        var flag = -1
+        for (i in s.indices) {
+            z[index].append(charArray[i])
+            if (index == 0 || index == numRows - 1) {
+                flag = -flag
             }
-            println()
+            index += flag
+        }
+
+        val stringBuffer = StringBuffer()
+        for (row in z) {
+            stringBuffer.append(row)
         }
         return stringBuffer.toString()
     }
